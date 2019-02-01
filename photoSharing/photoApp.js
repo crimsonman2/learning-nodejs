ae75bf09250689db8d3ab483081468798fa283ae
crimsonman2 <crimsonman2@gmail.com>
2019-02-01 16:29:00 +0600

    vl:: added POST support

--- photoSharing/photoApp.js
+++ photoSharing/photoApp.js
@@ -19,38 +19,81 @@ function handleRequest(req, res) {
         params = {};
 
     console.log(
-        'Incoming request: ' +
-        ' Headers: ' + JSON.stringify(req.headers) +
-        ' Method: ' + req.method +
-        ' URL: ' + req.url +
-        ' Path: ' + requestedUrl.pathname +
-        ' Raw Query: ' + JSON.stringify(rawQueryParams)
+        '----------------------- ------------------------------------------' + '\n' +
+        'Incoming request: ' + '\n' +
+        ' Headers: ' + JSON.stringify(req.headers) + '\n' +
+        ' Method: ' + req.method + '\n' +
+        ' URL: ' + req.url + '\n' +
+        ' Path: ' + requestedUrl.pathname + '\n' +
+        ' Raw Query: ' + JSON.stringify(rawQueryParams) + '\n' +
+        '-----------------------------------------------------------------'
     );
 
-    if (rawQueryParams) {
-        if (rawQueryParams[SHOW_FILES]) {
-            params.showFiles = (rawQueryParams[SHOW_FILES] === 'true');
-        }
-        if (rawQueryParams[PAGE_SIZE]) {
-            let pageSize = parseInt(rawQueryParams[PAGE_SIZE]);
-            params.pageSize = isNaN(pageSize) ? 25 : pageSize;
-        }
-        if (rawQueryParams[PAGE]) {
-            let page = parseInt(rawQueryParams[PAGE]) - 1;
-            params.page = isNaN(page) ? 0 : page;
-        }
-    }
+    switch (req.method) {
+        case "POST":
+            let json_data = '';
 
-    if (ALBUMS_ROOT.test(requestedUrl.pathname) || OTHER_ALBUMS.test(requestedUrl.pathname)) {
-        readAlbums(requestedUrl.pathname, params, (err, data) => {
-            if (err) {
-                sendFailure(res, 500, err);
-            } else {
-                sendSuccess(res, data);
+            //By default, no encoding is assigned and stream data will be returned as Buffer objects. Setting an encoding causes the stream data to be returned as strings of the specified encoding rather than as Buffer objects.
+            //The Readable stream will properly handle multi-byte characters delivered through the stream that would otherwise become improperly decoded if simply pulled from the stream as Buffer objects.
+            req.setEncoding('utf8');
+
+            //process incoming POST data
+            req.on('data',
+                (chunk) => {
+                //if chunk is string, if chunk is Buffer
+                //we set encoding before subscribing to stream events, so the chunk data will always be represented as String
+                    json_data += chunk;
+            });
+            req.on('end',
+                () => {
+                let output = '';
+                if (!json_data || json_data.length === 0) {
+                    output = "Nothing to process.";
+                    }
+                else {
+                    try {
+                        let json = JSON.parse(json_data);
+                        output = "Received JSON data: " + JSON.stringify(json);
+                    } catch (error) {
+                        output = "Error parsing JSON: " + error.message;
+                    }
+                }
+                res.end(output);
+            });
+            req.on('error',
+                (error) => {
+                //Error object goes here
+                    sendFailure(res, "error_processing_post_data", error);
+            });
+            break;
+
+        case "GET":
+        default:
+            if (rawQueryParams) {
+                if (rawQueryParams[SHOW_FILES]) {
+                    params.showFiles = (rawQueryParams[SHOW_FILES] === 'true');
+                }
+                if (rawQueryParams[PAGE_SIZE]) {
+                    let pageSize = parseInt(rawQueryParams[PAGE_SIZE]);
+                    params.pageSize = isNaN(pageSize) ? 25 : pageSize;
+                }
+                if (rawQueryParams[PAGE]) {
+                    let page = parseInt(rawQueryParams[PAGE]) - 1;
+                    params.page = isNaN(page) ? 0 : page;
+                }
             }
-        });
-    } else {
-        sendFailure(res, 404, {code:"BAD_REQUEST", message:"No such page"});
+
+            if (ALBUMS_ROOT.test(requestedUrl.pathname) || OTHER_ALBUMS.test(requestedUrl.pathname)) {
+                readAlbums(requestedUrl.pathname, params, (err, data) => {
+                    if (err) {
+                        sendFailure(res, 500, err);
+                    } else {
+                        sendSuccess(res, data);
+                    }
+                });
+            } else {
+                sendFailure(res, 404, {code:"BAD_REQUEST", message:"No such page"});
+            }
     }
 }
 
